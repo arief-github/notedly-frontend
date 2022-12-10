7a2a006d3598400aa20f211bf515ef992d24bbe3
github.arief <78076941+arief-github@users.noreply.github.com>
2022-12-10 10:29:05 +0700

    add : feature of pagination

--- src/pages/home.js
+++ src/pages/home.js
@@ -31,7 +31,37 @@ const Home = () => {
     if (error) return <p>Error!</p>
 
     return (
-    	<NoteFeed notes={data.noteFeed.notes} />
+    	<>
+    		 <NoteFeed notes={data.noteFeed.notes} />
+		 {data.noteFeed.hasNextPage && (
+		        <Button
+		          onClick={() =>
+		            fetchMore({
+		              variables: {
+		                cursor: data.noteFeed.cursor
+		              },
+		              updateQuery: (previousResult, { fetchMoreResult }) => {
+		                return {
+		                  noteFeed: {
+		                    cursor: fetchMoreResult.noteFeed.cursor,
+		                    hasNextPage: fetchMoreResult.noteFeed.hasNextPage,
+		                    // combine the new results and the old
+		                    notes: [
+		                      ...previousResult.noteFeed.notes,
+		                      ...fetchMoreResult.noteFeed.notes
+		                    ],
+		                    __typename: 'noteFeed'
+		                  }
+		                };
+		              }
+		            })
+		          }
+		        >
+		          Load more
+		        </Button>
+		      )}
+    	</>
+      	
     )
 }
 
