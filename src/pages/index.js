a736a16fc389dadcde28d6468ae818611f959b07
github.arief <78076941+arief-github@users.noreply.github.com>
2022-12-11 23:15:17 +0700

    add : sign Up (mutation) feature

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Home from './home';
 import MyNotes from './mynotes';
 import Favorites from './favorites';
 import NotePage from './note';
+import SignUp from './signup';
 
 import Layout from '../components/Layout';
 
@@ -15,6 +16,7 @@ const Pages = (props) => {
 						<Route path="/mynotes" component={MyNotes}/>
 						<Route path="/favorites" component={Favorites}/>	
 						<Route path="/note/:id" component={NotePage}/>
+						<Route path="/signup" component={SignUp}/>
 					</Layout>
 			</Router>
 	)
---
+++ src/pages/signup.js
@@ -0,0 +1,100 @@
+import React, { useEffect, useState } from 'react';
+import { useMutation, useApolloClient, gql } from '@apollo/client';
+import styled from 'styled-components';
+
+import Button from '../components/Button';
+
+const Wrapper = styled.div `
+	border: 1px solid #f5f4f0;
+	max-width: 500px;
+	padding: 1em;
+	margin: 0 auto;
+`;
+
+const Form = styled.form `
+	label,
+	input {
+		display: block;
+		line-height: 2em;
+	}
+
+	input {
+		width: 100%;
+		margin-bottom: 1em;
+	}
+`
+
+const SIGNUP_USER = gql `
+  mutation signUp($email: String!, $username: String!, $password: String!) {
+    signUp(email: $email, username: $username, password: $password)
+  }
+`;
+
+const SignUp = (props) => {
+    const [values, setValues] = useState();
+
+    const onChange = (event) => {
+        setValues({
+            ...values,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    useEffect(() => {
+        document.title = 'Sign Up - Notedly';
+    });
+
+    const [signUp, { loading, error }] = useMutation(SIGNUP_USER, {
+        onCompleted: data => {
+            // console.log the JSON Web Token when the mutation is complete
+            console.log(data.signUp);
+            localStorage.setItem('token', data.signUp);
+            props.history.push('/');
+        }
+    });
+
+    return (
+        <Wrapper>
+			<h2>Sign Up</h2>
+			<Form
+			    onSubmit={event => {
+			    event.preventDefault();
+			    signUp({
+			            variables: {
+			              ...values
+			            }
+			          });
+			        }}
+			      >
+				<label htmlFor="username">Username:</label>
+	        <input
+	          required
+	          type="text"
+	          name="username"
+	          placeholder="username"
+	          onChange={onChange}
+	        />
+	        <label htmlFor="email">Email:</label>
+	        <input
+	          required
+	          type="email"
+	          name="email"
+	          placeholder="Email"
+	          onChange={onChange}
+	        />
+	        <label htmlFor="password">Password:</label>
+	        <input
+	          required
+	          type="password"
+	          name="password"
+	          placeholder="Password"
+	          onChange={onChange}
+	        />
+	        <Button type="submit">Submit</Button>
+
+				</Form>
+		</Wrapper>
+    )
+};
+
+export default SignUp;
