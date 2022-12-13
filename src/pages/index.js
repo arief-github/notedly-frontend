319724fcdcc2faa047ef28804e26b3440c87e557
github.arief <78076941+arief-github@users.noreply.github.com>
2022-12-13 22:48:05 +0700

    add : PrivateRoute Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect ,Switch } from 'react-router-dom';
+import { useQuery, gql } from '@apollo/client';
 import Home from './home';
 import MyNotes from './mynotes';
 import Favorites from './favorites';
@@ -9,11 +10,19 @@ import SignIn from './signin';
 
 import Layout from '../components/Layout';
 
+const IS_LOGGED_IN = gql`
+	{
+		isLoggedIn @client
+	}
+`;
+
 const Pages = (props) => {
 	return (
 			<Router>
 					<Layout>
 						<Route exact path="/" component={Home}/>
+						<PrivateRoute path="/mynotes" component={MyNotes}/>
+						<PrivateRoute path="/favorites" component={Favorites}/>
 						<Route path="/mynotes" component={MyNotes}/>
 						<Route path="/favorites" component={Favorites}/>	
 						<Route path="/note/:id" component={NotePage}/>
@@ -23,5 +32,30 @@ const Pages = (props) => {
 			</Router>
 	)
 }
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const { loading, error, data } = useQuery(IS_LOGGED_IN);
+  // if the data is loading, display a loading message
+  if (loading) return <p>Loading...</p>;
+  // if there is an error fetching the data, display an error message
+  if (error) return <p>Error!</p>;
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        data.isLoggedIn === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/signin',
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
+};
 
 export default Pages;
